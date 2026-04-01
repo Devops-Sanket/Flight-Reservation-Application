bb51ae8a26d4a22ceeffb88a675643355b7766c3
Sanket_Patil <webdesigner0023@gmail.com>
2026-04-01 15:12:05 +0530

    Refactor pipeline stages and improve formatting

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -1,41 +1,37 @@
-pipeline{
-    agent any
-    stages{
-        stage('Code-pull'){
+pipeline {
+    agent any 
+    stages {
+        stage('Code-Pull'){
             steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'    
             }
-
         }
         stage('Code-Build'){
             steps{
                 sh '''
                     cd FlightReservationApplication
-                    mvn clean package
+                    mvn clean package 
                 '''
             }
-
         }
-        stage('Code-Test'){
+        stage('QA-Test'){
             steps{
-               withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'sonar-token') {
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
                  sh '''
                     cd FlightReservationApplication
                     mvn sonar:sonar -Dsonar.projectKey=flight-reservation
                  '''
                 }
             }
-
         }
         stage('Docker-Build'){
             steps{
                 sh '''
                     cd FlightReservationApplication
-                    docker build -t sanketx/flight-reservation:latest .
-                    docker push sanketx/flight-reservation:latest
-                    docker rmi sanketx/flight-reservation:latest
-
-                '''    
+                    docker build -t sanketx/flight-reservation:latest . 
+                    docker push sanketx/flight-reservation:latest 
+                    docker rmi sanketx/flight-reservation:latest 
+                '''
             }
         }
         stage('Deploy'){
